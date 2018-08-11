533ce4e626d0c89623f70d83289fb040475fd233
Mikhail Sotnikov <thehofch@gmail.com>
2018-08-11 21:12:44 +0300

    changes

--- photoschool/ViewController.swift
+++ photoschool/ViewController.swift
@@ -9,9 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var emailField: UITextField!
-    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet var emailField: UITextField!
+    @IBOutlet var passwordField: UITextField!
     @IBAction func logInTouch(_ sender: UIButton) {
         if let email = emailField.text, let pass = passwordField.text {
             sendAuthData(email: email, pass: pass)
@@ -19,34 +18,44 @@ class ViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    struct Auth: Codable {
+        var user: User
+        var auth_token: AuthToken
     }
-    
-    func sendAuthData(email: String, pass: String){
-        let json: [String: Any] = ["auth":["email": email, "password": pass]]
-        if let jsonData = try? JSONSerialization.data(withJSONObject: json) {
-            var request = URLRequest(url: URL(string: "https://highiso.photo/user_token")!)
-            request.httpMethod = "POST"
-            request.addValue("application/json", forHTTPHeaderField: "Accept")
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.httpBody = jsonData
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                guard let data = data, error == nil else {
-                    print(error?.localizedDescription ?? "No data")
-                    return
-                }
-                let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
-                if let responseJSON = responseJSON as? [String: Any] {
-                    print(responseJSON)
-                }
-            }
-            
-            task.resume()
+    struct User: Codable {
+        var email: String
+    }
+    struct AuthToken: Codable {
+        var payload: Payload
+        var token: String
+    }
+    struct Payload: Codable {
+        var exp: Int
+        var sub: Int
+    }
+    private func sendAuthData(email: String, pass: String){
+        let json: [String: Any] = [
+            "auth": [
+                "email": email,
+                "password": pass
+            ]
+        ]
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: json) else {
+            return
         }
+        var request = URLRequest(url: URL(string: "https://highiso.photo/user_token")!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "No data")
+                return
+            }
+            print(data)
+        }
+        task.resume()
     }
 }
