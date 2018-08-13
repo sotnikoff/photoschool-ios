e4cc152719cfe4d277e6e079a01a08068519a7d6
Mikhail Sotnikov <thehofch@gmail.com>
2018-08-13 15:35:52 +0300

    load courses

--- photoschool/MainViewController.swift
+++ photoschool/MainViewController.swift
@@ -9,10 +9,42 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    struct Course: Codable {
+        var id: Int
+        var title: String
+        var description: String
+        var text: String
+        var image: String
+        var featured: Bool
+        var created_at: String
+        var updated_at: String
+    }
+    
+    struct FailableDecodable<Base : Decodable> : Decodable {
+        let base: Base?
+        init(from decoder: Decoder) throws {
+            let container = try decoder.singleValueContainer()
+            self.base = try? container.decode(Base.self)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        var request = URLRequest(url: URL(string: "https://highiso.photo/api/courses")!)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "No data")
+                return
+            }
+            let decoder = JSONDecoder()
+            let courses = try! decoder.decode([FailableDecodable<Course>].self, from: data)
+            print(courses)
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
